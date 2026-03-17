b36092d21343dec0186ed6e99dd66542862fed7c
Jason <jason.tsang@decoder.hk>
2026-03-18 07:24:32 +0800

    add remaining

---
+++ withRole/roles/webserver/templates/index.html.js
@@ -0,0 +1,15 @@
+<html>
+
+  <head><title>{{ index_title }}</title></head>
+
+  <body>
+
+    <h1>{{ app_name }}</h1>
+
+    <p>Host: {{ inventory_hostname }}</p>
+
+    <p>App port: {{ app_port }}</p>
+
+  </body>
+
+</html>
